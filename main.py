80b700dddb32cc407045760dd5d44e14ca370107
edmar.pires9 <edmarpires18@gmail.com>
2024-07-26 11:17:17 -0300

    Programa está pronto para ler e salvar a tabela do Excel dentro de uma variavel

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-#Importação das bibliotecas necessárias para o projeto
+#Importação das bibliotecas
+#Panda uma excelente alternativa rápida e confiavél para realizar a leitura de tabelas do Excel.
 import pandas as pd
+#Matplotlib uma ferramenta para gerar gráficos com facilidade
 import matplotlib.pyplot as plt
+# Função para ler o arquivo
+def ler_tabela(caminho_do_arquivo):
+    if caminho_do_arquivo.endswith('.csv'):
+        tabela = pd.read_csv(caminho_do_arquivo)
+    elif caminho_do_arquivo.endswith('xlsx'):
+        tabela = pd.read_excel(caminho_do_arquivo, engine='openpyxl')
+    else:
+        raise ValueError('Erro de leitura o arquivo selecionado não corresponde aos formatos suportados .CSV ou .XLSX.')
+    return tabela
+
+
